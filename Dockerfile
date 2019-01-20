06dfc1925ad056740ee23fb578c51fb3d67465c3
Arnab <arniesaha@gmail.com>
2019-01-20 14:46:55 +0530

    redis config updates

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM java:8
+VOLUME /tmp
+ADD target/com.vanhack-0.0.1.jar app.jar
+RUN bash -c 'touch /app.jar'
 EXPOSE 8080
-ADD /target/com.vanhack-0.0.1.jar com.vanhack-0.0.1.jar
-ENTRYPOINT ["java", "-jar", "com.vanhack-0.0.1.jar"]
+ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
--- src/main/java/com/vanhack/config/RedisConfig.java
+++ src/main/java/com/vanhack/config/RedisConfig.java
@@ -1,10 +1,8 @@
 package com.vanhack.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
@@ -21,11 +19,10 @@ public class RedisConfig {
 
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-    	JedisConnectionFactory factory = new JedisConnectionFactory();
-	    factory.setHostName("redis");
-	    factory.setPort(6379);
-	    factory.setUsePool(true);
-	    return factory;
+        JedisConnectionFactory factory = new JedisConnectionFactory();
+        factory.setHostName("redis");
+        factory.setPort(6379);
+        return factory;
     }
 
     @Bean
@@ -35,7 +32,6 @@ public class RedisConfig {
         template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
         return template;
     }
-    
 
     @Bean
     MessageListenerAdapter messageListener() {
